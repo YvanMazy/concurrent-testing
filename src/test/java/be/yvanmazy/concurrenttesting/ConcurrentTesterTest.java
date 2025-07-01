e1ba783831519a99743e704adb369c58ed04d136
YvanMazy <mazy.yvan@protonmail.com>
2025-07-01 14:06:32 +0200

    :white_check_mark: Add unit tests for exception propagation and deadlock timeout

--- src/test/java/be/yvanmazy/concurrenttesting/ConcurrentTesterTest.java
+++ src/test/java/be/yvanmazy/concurrenttesting/ConcurrentTesterTest.java
@@ -5,16 +5,22 @@ import be.yvanmazy.concurrenttesting.util.Counter;
 import be.yvanmazy.concurrenttesting.util.NonAtomicCounter;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.opentest4j.AssertionFailedError;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 // More of a demonstration than a real test
 class ConcurrentTesterTest {
 
     private static final int REPETITION = 10;
     private static final int THREADS = 100;
-    private static final int ITERATIONS = 1000;
+    private static final int ITERATIONS = 5000;
     private static final int EXPECTED = THREADS * ITERATIONS;
 
     @Test
@@ -41,9 +47,19 @@ class ConcurrentTesterTest {
     void testRunWithBarrierConsumer() {
         final Counter counter = new AtomicCounter();
         ConcurrentTester.run(barrier -> {
+            for (int i = 0; i < ITERATIONS; i++) {
+                counter.increment();
+            }
+            for (int i = 0; i < ITERATIONS; i++) {
+                counter.decrement();
+            }
             counter.increment();
             barrier.await();
             barrier.await();
+            for (int i = 0; i < ITERATIONS; i++) {
+                counter.increment();
+                counter.decrement();
+            }
             counter.decrement();
             barrier.await();
             barrier.await();
@@ -57,6 +73,24 @@ class ConcurrentTesterTest {
             barrier.await();
         }).threads(THREADS));
         assertEquals(THREADS, counter.get());
+    }
+
+    @Test
+    @Timeout(value = 5L, unit = TimeUnit.SECONDS, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    void testNoDeadlockOnWorkerThread() {
+        assertThrows(AssertionError.class, () -> ConcurrentTester.run(barrier -> {
+            throw new RuntimeException("Test");
+        }, c -> c.afterStart(CyclicBarrier::await).threads(THREADS)));
+    }
+
+    @Test
+    @Timeout(value = 15L, unit = TimeUnit.SECONDS, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    void testTimeoutOnDeadlock() {
+        final Lock lock = new ReentrantLock();
+        final AssertionFailedError error = assertThrowsExactly(AssertionFailedError.class, () -> {
+            ConcurrentTester.run(lock::lock, t -> t.threads(5).timeout(2L, TimeUnit.SECONDS));
+        });
+        assertEquals("Test timed out", error.getMessage());
     }
 
 }
